e318faa7ab10333d23675ba2753a5e745617d781
Hung Doan Chan <hungkevin19122001@gmail.com>
2021-08-29 10:46:53 +0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 11;
+export const KEY = 'f6f1e901-106b-4e09-8384-3d47f8a3c70e';
+export const MODAL_CLOSE_SEC = 3;
